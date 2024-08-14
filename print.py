0d8bdf042794d0730315658433dd7f78bbc090b7
Adham <ahelal@users.noreply.github.com>
2024-08-14 18:49:38 +0200

    Create print.py

---
+++ print.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+import sys
+import os
+
+# ANNOTATION_1=file=README.md,line=2,endLine=2,title='No content'::Read me is empty and should be filled.\nit should have more details.
+# ANNOTATION_2=file=.github/workflows/linter.yml,line=30,endLine=33,title='annotation'::HELLO tests2
+
+# loop for environment variables and find variable  starts with ANNOTATION_
+for env in os.environ:
+    if env.startswith("ANNOTATION_"):
+        print("")
+        print(f"::notice::{os.environ[env]}")
+        print("")
