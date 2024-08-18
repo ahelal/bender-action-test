b69897c84dbd840f002a9242803a705b79fbaabc
Adham <ahelal@users.noreply.github.com>
2024-08-18 15:47:09 +0200

    init

---
+++ main.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var escape = require('escape-html');
+var serialize = require('node-serialize');
+var app = express();
+app.use(cookieParser())
+
+app.get('/', function (req, res) {
+    if (req.cookies.profile) {
+        var str = new Buffer(req.cookies.profile, 'base64').toString();
+        var obj = serialize.unserialize(str);
+        if (obj.username) {
+            res.send("Hello " + escape(obj.username));
+        }
+    } else {
+        res.cookie('profile', "eyJ1c2VybmFtZSI6ImFqaW4iLCJjb3VudHJ5IjoiaW5kaWEiLCJjaXR5IjoiYmFuZ2Fsb3JlIn0=", {
+            maxAge: 900000,
+            httpOnly: true
+        });
+    }
+    res.send("Hello World");
+});
+
+app.post("/list-users", (req, res) => {
+    var obj = req.body.users;
+    var someArr = [];
+
+    for (var i = 0; i < obj.length; i++) {
+        someArr.push(obj[i]);
+    }
+
+    res.send(someArr.join(','));
+});
+
+app.listen(3000);
